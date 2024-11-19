c0e31b0740e9ba8de4681020321c35cabb428b7a
mrmoi31 <dodo.leva.dl@gmail.com>
2024-11-19 11:21:49 +0100

    feat: connect to metamask

--- sae_crypto/src/App.js
+++ sae_crypto/src/App.js
@@ -1,21 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
 
 function App() {
+  const connect = async () => {
+    let signer = null;
+    let provider;
+
+    if (!window.ethereum) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      console.log("MetaMask detected");
+      provider = new ethers.BrowserProvider(window.ethereum);
+
+      try {
+        // Request account access
+        await provider.send("eth_requestAccounts", []);
+        signer = await provider.getSigner();
+        console.log("Connected with signer:", signer);
+      } catch (error) {
+        console.error("User rejected connection", error);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> testestets
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={connect}>Connect to MetaMask</button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React
         </a>
       </header>
     </div>
