fee15fcb1bcd65e287aaa5bb523b84398247b3da
chihiroprog <stutuart.barthe@gmail.com>
2024-11-19 11:45:24 +0100

    pages

--- sae_crypto/src/App.js
+++ sae_crypto/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { ethers } from "ethers";
-
+import Navbar from './functions/navbar';
 function App() {
   const connect = async () => {
     let signer = null;
@@ -28,22 +28,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar />
+        
         <button onClick={connect}>Connect to MetaMask</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
 }
 
 export default App;
+
