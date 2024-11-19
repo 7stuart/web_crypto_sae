b2fc192c5a5aba8340056f6279537ed7b75dab5d
chihiroprog <stutuart.barthe@gmail.com>
2024-11-19 11:23:01 +0100

    Revert "creations des pages"
    This reverts commit 93e9ac818147387b091aebba00328723f6c7334c.

--- sae_crypto/src/App.js
+++ sae_crypto/src/App.js
@@ -1,13 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import './pages/home.js'
-import './pages/loterie.js'
-import { useState } from 'react';
+
 function App() {
   return (
-    <div> 
-     <button>home</button> 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> testestets
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React 
+        </a>
+      </header>
     </div>
   );
-} 
+}
 
 export default App;
