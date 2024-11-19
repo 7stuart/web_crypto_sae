115b8d56baeeeba98e219b7cf929550e0e7c6bd7
chihiroprog <stutuart.barthe@gmail.com>
2024-11-19 11:45:34 +0100

    pages

---
+++ sae_crypto/src/functions/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../css/navbar.css';
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+          <nav className="navbar">
+
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/home">Home</a>
+      </li>
+      <li>
+        <a href="/lottery">lottery</a>
+      </li>
+      <li>
+        <a href="/connect">Connect</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        </nav>
+    );
+};
+
+export default Navbar;
