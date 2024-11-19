93e9ac818147387b091aebba00328723f6c7334c
chihiroprog <stutuart.barthe@gmail.com>
2024-11-19 11:19:33 +0100

    creations des pages

--- sae_crypto/src/App.js
+++ sae_crypto/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './pages/home.js'
+import './pages/loterie.js'
+import { useState } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> testestets
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+    <div> 
+     <button>home</button> 
     </div>
   );
-}
+} 
 
 export default App;
