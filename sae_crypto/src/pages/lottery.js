bfc8a3398366f495457a7ea4ce27b6a859917c36
Dardet <lenny.dardet@etu-iut.tlse3.fr>
2024-11-20 08:19:20 +0100

    page loterie

--- sae_crypto/src/pages/lottery.js
+++ sae_crypto/src/pages/lottery.js
@@ -1,7 +1,21 @@
-function Lottery({redirection : any}) {
-    
-    return(
-        <div> aa  </div>
-)
+import React, { useState } from 'react';
+
+function Lottery() {
+  const [number, setNumber] = useState(null); // État pour stocker le chiffre tiré
+
+  const performDraw = () => {
+    // Générer un chiffre aléatoire entre 1 et 10
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    setNumber(randomNumber); // Mettre à jour l'état avec ce chiffre
+  };
+
+  return (
+    <div>
+      <h1>Lottery Test</h1>
+      <button onClick={performDraw}>Effectuer le tirage</button>
+      {number && <p>Chiffre tiré : {number}</p>}
+    </div>
+  );
 }
+
 export default Lottery;
